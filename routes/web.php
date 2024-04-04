8880951ca2623e9b3bdf1dbf7ba5f177e23d216c
xllyyyyy <johnellyaltarejos69@gmail.com>
2024-04-04 11:52:18 +0800

    Add posts

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts.index');
 });
+
+Route::get('/posts/{post}', function ($post) {
+    return view('posts.show', [
+        'post' => $post
+    ]);
+});
+
